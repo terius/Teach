96d0e1045d62fde43aa3e8c1935a0645d83d257c
teriu <teriu@DESKTOP-11706O7>
2017-04-29 09:18:05 +0800

    斤斤计较

---
+++ NewTeacher/Form1.cs
@@ -0,0 +1,21 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using System.Drawing;
+
+namespace NewTeacher
+{
+    public partial class Form1 : MaterialForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Green600, Primary.Green700, Primary.Green200, Accent.Red100, TextShade.WHITE);
+        //    materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+            this.materialLabel1.ForeColor = Color.White;
+        }
+    }
+}
