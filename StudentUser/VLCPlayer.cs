98ce5c72f5b226c6cefaed8d111f5757505e8804
teriusstar <18728856@qq.com>
2017-05-03 17:59:58 +0800

    aaaaaaa

---
+++ StudentUser/VLCPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentUser
+{
+    public partial class VLCPlayer : Form
+    {
+        public VLCPlayer()
+        {
+            InitializeComponent();
+        }
+
+        private void VLCPlayer_Load(object sender, EventArgs e)
+        {
+            string pluginPath = Environment.CurrentDirectory + "\\plugins\\";  //插件目录
+            var player = new VlcPlayerBase(pluginPath);
+            player.SetRenderWindow((int)this.Handle);//panel
+            player.LoadFile(@"E:\terius\hkdg.mkv");//视频文件路径
+        }
+    }
+}
